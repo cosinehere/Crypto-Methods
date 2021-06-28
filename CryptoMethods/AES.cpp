1ee5fbaf0e25ba51308baf2f84021da2fdecaa1f
cosinehere <975745601@qq.com>
2021-06-28 17:26:16 +0800

    Complete AES Class

--- CryptoMethods/AES.cpp
+++ CryptoMethods/AES.cpp
@@ -3,35 +3,254 @@
 
 NAMESPACEBEGIN(CryptoMethods)
 
+constexpr uint32_t c_aesblocksize = 16;
+
+inline uint8_t gmult(uint8_t a, uint8_t b) {
+	uint8_t p = 0, hbs = 0;
+	uint8_t tempa = a, tempb = b;
+	for (size_t i = 0; i < 8; i++) {
+		if (b & 1)
+			p ^= a;
+
+		hbs = a & 0x80;
+		a <<= 1;
+		if (hbs) a ^= 0x1b;   
+		b >>= 1;
+	}
+
+	return (uint8_t)p;
+}
+
+inline void subbytes(uint8_t* state, const uint8_t* box)
+{
+	for (size_t i = 0; i < 16; ++i)
+	{
+		state[i] = box[state[i]];
+	}
+}
+
+inline void shiftrows(uint8_t* state)
+{
+	uint8_t tmp = state[1];
+	state[1] = state[5];
+	state[5] = state[9];
+	state[9] = state[13];
+	state[13] = tmp;
+
+	tmp = state[2];
+	state[2] = state[10];
+	state[10] = tmp;
+	tmp = state[6];
+	state[6] = state[14];
+	state[14] = tmp;
+
+	tmp = state[3];
+	state[3] = state[15];
+	state[15] = state[11];
+	state[11] = state[7];
+	state[7] = tmp;
+}
+
+inline void rshiftrows(uint8_t* state)
+{
+	uint8_t tmp = state[13];
+	state[13] = state[9];
+	state[9] = state[5];
+	state[5] = state[1];
+	state[1] = tmp;
+
+	tmp = state[2];
+	state[2] = state[10];
+	state[10] = tmp;
+	tmp = state[6];
+	state[6] = state[14];
+	state[14] = tmp;
+
+	tmp = state[7];
+	state[7] = state[11];
+	state[11] = state[15];
+	state[15] = state[3];
+	state[3] = tmp;
+}
+
+inline void mixcolumns(uint8_t* state)
+{
+	uint8_t tmp[4];
+	for (size_t i = 0; i < 4; ++i)
+	{
+		tmp[0] = gmult(2, state[4 * i]) ^ gmult(3, state[4 * i + 1]) ^ gmult(1, state[4 * i + 2]) ^ gmult(1, state[4 * i + 3]);
+		tmp[1] = gmult(1, state[4 * i]) ^ gmult(2, state[4 * i + 1]) ^ gmult(3, state[4 * i + 2]) ^ gmult(1, state[4 * i + 3]);
+		tmp[2] = gmult(1, state[4 * i]) ^ gmult(1, state[4 * i + 1]) ^ gmult(2, state[4 * i + 2]) ^ gmult(3, state[4 * i + 3]);
+		tmp[3] = gmult(3, state[4 * i]) ^ gmult(1, state[4 * i + 1]) ^ gmult(1, state[4 * i + 2]) ^ gmult(2, state[4 * i + 3]);
+		state[4 * i] = tmp[0];
+		state[4 * i + 1] = tmp[1];
+		state[4 * i + 2] = tmp[2];
+		state[4 * i + 3] = tmp[3];
+	}
+}
+
+inline void rmixcolumns(uint8_t* state)
+{
+	uint8_t tmp[4];
+	for (size_t i = 0; i < 4; ++i)
+	{
+		tmp[0] = gmult(14, state[4 * i]) ^ gmult(11, state[4 * i + 1]) ^ gmult(13, state[4 * i + 2]) ^ gmult(9, state[4 * i + 3]);
+		tmp[1] = gmult(9, state[4 * i]) ^ gmult(14, state[4 * i + 1]) ^ gmult(11, state[4 * i + 2]) ^ gmult(13, state[4 * i + 3]);
+		tmp[2] = gmult(13, state[4 * i]) ^ gmult(9, state[4 * i + 1]) ^ gmult(14, state[4 * i + 2]) ^ gmult(11, state[4 * i + 3]);
+		tmp[3] = gmult(11, state[4 * i]) ^ gmult(13, state[4 * i + 1]) ^ gmult(9, state[4 * i + 2]) ^ gmult(14, state[4 * i + 3]);
+		state[4 * i] = tmp[0];
+		state[4 * i + 1] = tmp[1];
+		state[4 * i + 2] = tmp[2];
+		state[4 * i + 3] = tmp[3];
+	}
+}
+
+inline void addroundkey(uint8_t* state, const uint8_t* word)
+{
+	for (size_t i = 0; i < 16; ++i)
+	{
+		state[i] ^= word[i];
+	}
+}
+
 AES::AES()
-{}
+{
+	p_haskey = false;
+}
 
 AES::~AES()
 {}
 
-bool AES::SetMode(enum_crypt_mode mode)
+bool AES::SetKey(const uint8_t* key, const size_t keylen)
 {
-	return false;
+	if (key == nullptr || (keylen != 16 && keylen != 24 && keylen != 32))
+	{
+		return false;
+	}
+
+	p_keylen = keylen;
+	memcpy_s(p_key, sizeof(uint8_t) * 32, key, sizeof(uint8_t) * keylen);
+
+	switch (p_keylen)
+	{
+	case 16:
+		p_rounds = 10;
+		break;
+	case 24:
+		p_rounds = 12;
+		break;
+	case 32:
+		p_rounds = 14;
+		break;
+	default: return false;
+	}
+	bool bRet = KeyExpand();
+	if (bRet)
+	{
+		p_haskey = true;
+	}
+	else
+	{
+		p_haskey = false;
+	}
+
+	return bRet;
 }
 
-bool AES::UpdateKey(const uint8_t* key, const size_t keylen)
+bool AES::Encrypt(const uint8_t* plain, uint8_t* cipher)
 {
-	return false;
-};
+	if (!p_haskey)
+	{
+		return false;
+	}
 
-bool AES::UpdateIV(const uint8_t* iv, const size_t ivlen)
-{
-	return false;
-};
+	memcpy_s(cipher, sizeof(uint8_t) * 16, plain, sizeof(uint8_t) * 16);
 
-bool AES::UpdateData(const uint8_t* data, const size_t datalen)
-{
-	return false;
-};
+	addroundkey(cipher, p_roundkey);
 
-bool AES::Finally(uint8_t* out, size_t* outlen)
+	for (size_t r = 1; r < p_rounds; ++r)
+	{
+		subbytes(cipher, SBox);
+		shiftrows(cipher);
+		mixcolumns(cipher);
+		addroundkey(cipher, p_roundkey + (r << 4));
+	}
+	subbytes(cipher, SBox);
+	shiftrows(cipher);
+	addroundkey(cipher, p_roundkey + (p_rounds << 4));
+
+	return true;
+}
+
+bool AES::Decrypt(const uint8_t* cipher, uint8_t* plain)
 {
-	return false;
-};
+	if (!p_haskey)
+	{
+		return false;
+	}
+
+	memcpy_s(plain, sizeof(uint8_t) * 16, cipher, sizeof(uint8_t) * 16);
+
+	addroundkey(plain, p_roundkey + (p_rounds << 4));
+
+	for (size_t r = p_rounds - 1; r > 0; --r)
+	{
+		rshiftrows(plain);
+		subbytes(plain, RSBox);
+		addroundkey(plain, p_roundkey + (r << 4));
+		rmixcolumns(plain);
+	}
+	rshiftrows(plain);
+	subbytes(plain, RSBox);
+	addroundkey(plain, p_roundkey);
+
+	return true;
+}
+
+bool AES::KeyExpand()
+{
+	p_roundkeylen = (p_rounds + 1) << 2;
+
+	memcpy_s(p_roundkey, sizeof(uint8_t)*p_keylen, p_key, sizeof(uint8_t)*p_keylen);
+
+	uint32_t mod = p_keylen >> 2;
+	for (size_t i = mod; i < p_roundkeylen; ++i)
+	{
+		uint8_t temp[4] = { p_roundkey[(i - 1) << 2],p_roundkey[((i - 1) << 2) + 1],
+							p_roundkey[((i - 1) << 2) + 2],p_roundkey[((i - 1) << 2) + 3] };
+		if (!(i % mod))
+		{
+			uint8_t tmp = temp[0];
+			temp[0] = temp[1];
+			temp[1] = temp[2];
+			temp[2] = temp[3];
+			temp[3] = tmp;
+
+			temp[0] = SBox[temp[0]];
+			temp[1] = SBox[temp[1]];
+			temp[2] = SBox[temp[2]];
+			temp[3] = SBox[temp[3]];
+
+			temp[0] ^= Rcon[i / mod - 1][0];
+			temp[1] ^= Rcon[i / mod - 1][1];
+			temp[2] ^= Rcon[i / mod - 1][2];
+			temp[3] ^= Rcon[i / mod - 1][3];
+		}
+		else if ((mod == 8) && ((i % mod) == 4))
+		{
+			temp[0] = SBox[temp[0]];
+			temp[1] = SBox[temp[1]];
+			temp[2] = SBox[temp[2]];
+			temp[3] = SBox[temp[3]];
+		}
+
+		p_roundkey[i << 2] = temp[0] ^ p_roundkey[(i - mod) << 2];
+		p_roundkey[(i << 2) + 1] = temp[1] ^ p_roundkey[((i - mod) << 2) + 1];
+		p_roundkey[(i << 2) + 2] = temp[2] ^ p_roundkey[((i - mod) << 2) + 2];
+		p_roundkey[(i << 2) + 3] = temp[3] ^ p_roundkey[((i - mod) << 2) + 3];
+	}
+
+	return true;
+}
 
 NAMESPACEEND
