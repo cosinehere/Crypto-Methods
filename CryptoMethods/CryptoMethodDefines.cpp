db6f95af73af83bc3546c82351849cba0f4c9d2c
cosinehere <975745601@qq.com>
2021-06-29 17:05:58 +0800

    Update interface defines

--- CryptoMethods/CryptoMethodDefines.cpp
+++ CryptoMethods/CryptoMethodDefines.cpp
@@ -2,17 +2,34 @@
 #include "CryptoMethodDefines.h"
 
 #include "AES.h"
+#include "CBC.h"
+#include "CFB.h"
+#include "CTR.h"
+
+#include <wincrypt.h>
 
 NAMESPACEBEGIN(CryptoMethods)
 
-void Padding(uint8_t* buffer, size_t len, size_t blocksize)
+size_t Padding(uint8_t* buffer, size_t len, size_t blocksize)
 {
 	size_t left = blocksize - (len % blocksize);
+	size_t lenret = len;
 	if (left == 0) left += blocksize;
 	for (size_t i = 0; i < left; ++i)
 	{
 		buffer[len + i] = static_cast<uint8_t>(left);
+		lenret++;
 	}
+
+	return lenret;
+}
+
+void GenerateIV(uint8_t* iv, size_t ivlen)
+{
+	HCRYPTPROV crypt;
+	CryptAcquireContext(&crypt, nullptr, nullptr, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
+	CryptGenRandom(crypt, ivlen, iv);
+	CryptReleaseContext(crypt, 0);
 }
 
 void CreateAES(CipherBase*& base)
@@ -24,6 +41,87 @@ void ReleaseAES(CipherBase*& base)
 {
 	delete base;
 	base = nullptr;
+}
+
+void AESCBCEncrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen)
+{
+	CBC<AES> cbc;
+	cbc.SetKey(key, keylen);
+	uint8_t iv[16]; //= { '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0' };
+	GenerateIV(iv, 16);
+	cbc.SetIV(iv, 16);
+
+	uint8_t* temp = new uint8_t[inlen + 16];
+	memcpy_s(temp, sizeof(uint8_t)*(inlen + 16), in, sizeof(uint8_t)*inlen);
+	size_t templen = PKCS7(temp, inlen, 16);
+
+	cbc.Encrypt(temp, templen, out, outlen);
+
+	delete[] temp;
+}
+
+void AESCBCDecrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen)
+{
+	CBC<AES> cbc;
+	cbc.SetKey(key, keylen);
+	uint8_t iv[16]; //= { '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0' };
+	GenerateIV(iv, 16);
+	cbc.SetIV(iv, 16);
+	cbc.Decrypt(in, inlen, out, outlen);
+}
+
+void AESCFBEncrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen)
+{
+	CFB<AES> cfb;
+	cfb.SetKey(key, keylen);
+	uint8_t iv[16] = { '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0' };
+	//GenerateIV(iv, 16);
+	cfb.SetIV(iv, 16);
+
+	uint8_t* temp = new uint8_t[inlen];
+	memcpy_s(temp, sizeof(uint8_t)*inlen, in, sizeof(uint8_t)*inlen);
+	size_t templen = inlen;
+
+	cfb.Encrypt(temp, templen, out, outlen);
+
+	delete[] temp;
+}
+
+void AESCFBDecrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen)
+{
+	CFB<AES> cfb;
+	cfb.SetKey(key, keylen);
+	uint8_t iv[16] = { '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0' };
+	//GenerateIV(iv, 16);
+	cfb.SetIV(iv, 16);
+	cfb.Decrypt(in, inlen, out, outlen);
+}
+
+void AESCTREncrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen)
+{
+	CTR<AES> cbc;
+	cbc.SetKey(key, keylen);
+	uint8_t iv[16] = { '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0' };
+	//GenerateIV(iv, 16);
+	cbc.SetIV(iv, 16);
+
+	uint8_t* temp = new uint8_t[inlen];
+	memcpy_s(temp, sizeof(uint8_t)*inlen, in, sizeof(uint8_t)*inlen);
+	size_t templen = inlen;
+
+	cbc.Encrypt(temp, templen, out, outlen);
+
+	delete[] temp;
+}
+
+void AESCTRDecrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen)
+{
+	CTR<AES> cbc;
+	cbc.SetKey(key, keylen);
+	uint8_t iv[16] = { '0','0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0','0' };
+	//GenerateIV(iv, 16);
+	cbc.SetIV(iv, 16);
+	cbc.Decrypt(in, inlen, out, outlen);
 }
 
 NAMESPACEEND
--- CryptoMethods/CryptoMethodDefines.h
+++ CryptoMethods/CryptoMethodDefines.h
@@ -15,10 +15,14 @@
 
 NAMESPACEBEGIN(CryptoMethods)
 
-CRYPTOEXT void Padding(uint8_t* buffer, size_t len, size_t blocksize);
+CRYPTOEXT size_t Padding(uint8_t* buffer, size_t len, size_t blocksize);
+
+CRYPTOEXT void GenerateIV(uint8_t* iv, size_t ivlen);
 
 #define PKCS7(buffer,len,blocksize) Padding(buffer,len,blocksize)
 #define PKCS5(buffer,len) Padding(buffer,len,8)
+
+constexpr uint8_t c_iv[] = { 0x5eu,0x95u,0x7cu,0xe3u,0x2bu,0x79u,0xa1u,0xf9u,0x35u,0x17u,0x8eu,0xdau,0x6cu,0xdeu,0x1du,0x2fu };
 
 enum enum_crypt_mode
 {
@@ -29,6 +33,7 @@ enum enum_crypt_mode
 class NOVTABLE CipherBase
 {
 public:
+	virtual const size_t BlockSize() = 0;
 	virtual bool SetKey(const uint8_t* key, const size_t keylen) = 0;
 	virtual bool Encrypt(const uint8_t* plain, uint8_t* cipher) = 0;
 	virtual bool Decrypt(const uint8_t* cipher, uint8_t* plain) = 0;
@@ -39,8 +44,11 @@ public:
 class NOVTABLE CipherModeBase
 {
 public:
-	virtual bool Encrypt() = 0;
-	virtual bool Decrypt() = 0;
+	virtual bool SetKey(const uint8_t* key, const size_t keylen) = 0;
+	virtual bool SetIV(const uint8_t* iv, const size_t ivlen) = 0;
+
+	virtual bool Encrypt(const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen) = 0;
+	virtual bool Decrypt(const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen) = 0;
 
 	virtual ~CipherModeBase() {}
 };
@@ -58,7 +66,15 @@ inline T l_rot(T a, T b)
 }
 
 CRYPTOEXT void CreateAES(CipherBase*& base);
-
 CRYPTOEXT void ReleaseAES(CipherBase*& base);
+
+CRYPTOEXT void AESCBCEncrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen);
+CRYPTOEXT void AESCBCDecrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen);
+
+CRYPTOEXT void AESCFBEncrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen);
+CRYPTOEXT void AESCFBDecrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen);
+
+CRYPTOEXT void AESCTREncrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen);
+CRYPTOEXT void AESCTRDecrypt(const uint8_t* key, const size_t keylen, const uint8_t* in, const size_t inlen, uint8_t* out, size_t& outlen);
 
 NAMESPACEEND
